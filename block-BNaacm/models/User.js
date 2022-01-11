629db7854d6b4b71da1cf23718c26410f3fd2877
Reena Mendonsa <reenamendonsa@gmail.com>
2022-01-11 17:37:46 +0530

    completed

---
+++ block-BNaacm/models/User.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var userSchema = new Schema(
+  {
+    name: String,
+    email: { type: String, lowercase: true, trim: true, match: /@/ },
+    age: Number,
+    favorites: [String],
+    marks: [Number],
+    password: { type: String, minlength: 5, maxlength: 15 },
+    createdAt: { type: Date, default: Date.now() },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
